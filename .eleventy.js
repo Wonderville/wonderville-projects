b76d96380859ad526610e9286d313cd27fb295ba
ty cobb <bin@tycobb.club>
2023-04-22 19:07:08 -0400

    stretch/center content on desktop/mobile

--- .eleventy.js
+++ .eleventy.js
@@ -11,6 +11,14 @@ module.exports = function (config) {
   config.addPlugin(PostCSSPlugin)
   config.addPassthroughCopy(`${srcDir}/img`)
 
+  // -- filters --
+  config.addFilter("camelize", (value) =>
+    value
+      .split(/\s+/)
+      .map((s) => s.slice(0, 1).toUpperCase() + s.slice(1, s.length))
+      .join("")
+  )
+
   // -- build --
   // remove the _collections dir from the site output
   config.on("eleventy.after", async () => {
