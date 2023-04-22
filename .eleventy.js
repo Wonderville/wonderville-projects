b34bad3fb9a38446d0d2bbd13e643727e81d9dcf
ty cobb <bin@tycobb.club>
2023-04-22 17:49:41 -0400

    remove _collections from output

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,5 @@
 const PostCSSPlugin = require("eleventy-plugin-postcss")
+const { rm } = require("fs/promises")
 
 // -- config --
 module.exports = function (config) {
@@ -9,6 +10,12 @@ module.exports = function (config) {
   // -- assets --
   config.addPlugin(PostCSSPlugin)
   config.addPassthroughCopy(`${srcDir}/img`)
+
+  // -- build --
+  // remove the _collections dir from the site output
+  config.on("eleventy.after", async () => {
+    await rm(`${dstDir}/_collections`, { recursive: true, force: true })
+  })
 
   // -- output --
   return {
