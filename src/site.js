6f4092aea6f49bd832e62ff253e65fbb3a478168
ty cobb <bin@tycobb.club>
2023-05-27 01:12:26 -0400

    add header logo & subtitle

--- src/site.js
+++ src/site.js
@@ -1,8 +1,55 @@
-const nav = document.getElementById("nav")
+// -- constants --
+const NAV_ANIM_MILLIS = 300
+const NAV_ATTR_STUCK = "stuck"
 
-const observer = new IntersectionObserver(
-  ([e]) => e.target.toggleAttribute("stuck", e.intersectionRatio < 1),
-  { threshold: [1] }
-)
+// -- props --
+let timeout = null
 
-observer.observe(nav)
+// -- main --
+function Main() {
+  // get elements
+  const $nav = document.getElementById("nav")
+
+  // bind events
+  const intersection = new IntersectionObserver(OnNavIntersectionChanged, {
+    threshold: [1],
+  })
+
+  intersection.observe($nav)
+}
+
+// -- commands --
+function StartNavTransition($el, start, finish) {
+  if ($el.getAttribute(NAV_ATTR_STUCK) == finish) {
+    return
+  }
+
+  if (timeout != null) {
+    clearTimeout(timeout)
+    timeout = null
+  }
+
+  SetNavStuck($el, start)
+  timeout = setTimeout(() => SetNavStuck($el, finish), NAV_ANIM_MILLIS)
+}
+
+function SetNavStuck($el, state) {
+  if (state == null) {
+    $el.removeAttribute(NAV_ATTR_STUCK)
+  } else {
+    $el.setAttribute(NAV_ATTR_STUCK, state)
+  }
+}
+
+// -- events --
+function OnNavIntersectionChanged([e]) {
+  const $el = e.target
+  if (e.intersectionRatio < 1) {
+    StartNavTransition($el, "enter", "")
+  } else {
+    StartNavTransition($el, "exit", null)
+  }
+}
+
+// -- bootstrap --
+Main()
